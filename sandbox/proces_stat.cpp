820c49649390e1a323158bb313e5460609bd84a6
Tamas Panyi <panyitamas@gmail.com>
2020-06-01 17:03:00 +0200

    sandbox

--- sandbox/proces_stat.cpp
+++ sandbox/proces_stat.cpp
@@ -12,6 +12,20 @@
 const std::string kProcDirectory{"/proc/"};
 const std::string kStatFilename{"/stat"};
 
+std::string Pad(std::string s, char c){
+  s.insert(s.begin(), 2 - s.size(), c);
+  return s;
+}
+
+std::string ElapsedTime(long seconds) {
+  int h = seconds / 3600;
+  int m = (seconds / 60) % 60;
+  int s = seconds % 60;
+  return std::string(Pad(std::to_string(h), '0') + ":" +
+                     Pad(std::to_string(m), '0') + ":" +
+                     Pad(std::to_string(s), '0'));
+}
+
 std::vector<std::string> ReadProcessStat(int pid) {
   std::vector<std::string> stat{};
   std::ifstream filestream(kProcDirectory + std::to_string(pid) + kStatFilename);
@@ -28,8 +42,7 @@ std::vector<std::string> ReadProcessStat(int pid) {
 
 int main (){
     std::vector<std::string> stat = ReadProcessStat(1509);
-    int i{0};
-    for (auto s : stat){
-        std::cout << i++ << ": " << s << "\n"; 
-    }
+    long int elapsed = stol(stat[22])/sysconf(_SC_CLK_TCK);
+    std::string formatted = ElapsedTime(elapsed);
+    std::cout << formatted <<"\n";
 }
